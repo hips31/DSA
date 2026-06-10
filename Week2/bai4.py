35e390d5511a19e3b9d3b8fc4d58bef5fd0de447
hips31 <vjzeol2004@gmail.com>
2026-06-10 12:54:21 +0700

    Add files via upload

---
+++ Week2/bai4.py
@@ -0,0 +1,20 @@
+def last_occurrence(a, x):
+    left, right = 0, len(a) - 1
+    ans = -1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if a[mid] == x:
+            ans = mid
+            left = mid + 1
+        elif a[mid] < x:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return ans
+
+a = list(map(int, input().split()))
+x = int(input())
+print(last_occurrence(a, x))
