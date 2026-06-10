ba15d1ffe2a1dbbb08961c2c3fdfe76ceced9adc
hips31 <vjzeol2004@gmail.com>
2026-06-10 12:51:43 +0700

    Add files via upload

---
+++ week2/Buoi2/bai1.py
@@ -0,0 +1,18 @@
+def binary_search(a, x):
+    left, right = 0, len(a) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if a[mid] == x:
+            return mid
+        elif a[mid] < x:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+a = list(map(int, input().split()))
+x = int(input())
+print(binary_search(a, x))
